b32ed996763a7bb5168252529f89900ac4d4d86c
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-09-05 15:15:19 +0200

    proof of concept works

--- profugus-example/src/main.rs
+++ profugus-example/src/main.rs
@@ -1,14 +1,10 @@
-use futures::{
-    future::Future,
-    stream::Stream
-};
 use tokio;
 use profugus::*;
 
 fn main() -> Result<(), Error> {
     let mut runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
     let conn = runtime.block_on(PGConnection::new("postgresql://localhost/dellstore2?user=tg"))?;
-    let number_of_products : Vec<Count> = runtime.block_on(conn.query("select count(id) as count from products"))?;
+    let number_of_products : Vec<Count> = runtime.block_on(conn.query("select count(*) as count from products"))?;
     dbg!(number_of_products);
     Ok(())
 }
