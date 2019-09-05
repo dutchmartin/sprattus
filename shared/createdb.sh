e213029221388da8db24e1e2e33428b2bcea5f86
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-09-05 15:05:10 +0200

    improve script

--- shared/createdb.sh
+++ shared/createdb.sh
@@ -5,6 +5,6 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 psql -U tg -h localhost -c "CREATE DATABASE dellstore2 OWNER tg TEMPLATE template0;"
 
-psql -U tg -h localhost -c "grant all privileges on database dellstore2 to myuser;"
+psql -U tg -h localhost -c "grant all privileges on database dellstore2 to tg;"
 
 psql dellstore2 < "${parent_path}/dellstore2-normal-1.0/dellstore2-normal-1.0.sql" -U tg -h localhost
