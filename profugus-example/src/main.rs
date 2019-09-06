55161196c48b8f037d863f12dffcd2f103e15df2
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-09-06 16:28:36 +0200

    friday changes

--- profugus-example/src/main.rs
+++ profugus-example/src/main.rs
@@ -4,12 +4,12 @@ use profugus::*;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let conn = PGConnection::new("postgresql://localhost/dellstore2?user=tg").await?;
-    let number_of_products : Vec<Count> = conn.query("select count(*) as count from products").await?;
+    let number_of_products : Vec<Count> = conn.query_multiple("select count(*) as count from products", &[]).await?;
     dbg!(number_of_products);
     Ok(())
 }
 
-#[derive(FromSql, Debug)]
+#[derive(FromSql, Debug, Default)]
 struct Count {
     count: i64
 }
--- profugus/src/lib.rs
+++ profugus/src/lib.rs
@@ -1,10 +1,12 @@
 use futures_util::try_future::TryFutureExt;
 use futures_util::future::FutureExt;
-use futures::TryStreamExt;
+use futures::{TryStreamExt, Future};
 use tokio_postgres::*;
 use parking_lot::*;
 use std::sync::Arc;
+use async_trait::*;
 use tokio;
+
 
 pub struct PGConnection {
     client: Arc<Mutex<Client>>
@@ -27,13 +29,13 @@ impl PGConnection {
             .map_err(|e| panic!("connection error: {}", e))
             .map(|conn|conn.unwrap());
         tokio::spawn(connection);
-        Ok(PGConnection{
+        Ok(PGConnection {
             client: Arc::new(Mutex::new(client))
         })
     }
 
-    pub async fn query<T>(self, sql: &str) -> Result<Vec<T>, Error>
-    where T: FromSql
+    pub async fn query_multiple<T>(self, sql: &str, args: &[&dyn ToSql]) -> Result<Vec<T>, Error>
+        where T: FromSql
     {
         let statement = self.client
             .lock()
@@ -41,16 +43,33 @@ impl PGConnection {
             .await?;
         self.client
             .lock()
-            .query(&statement, &[])
+            .query(&statement, args)
             .map_ok(|row|{
                 T::from_row(&row)
             })
             .try_collect::<Vec<T>>().await
     }
+// todo: Implement a proper single value return value.
+//    pub async fn query_single<T>(self, sql: &str, args: &[&dyn ToSql]) -> Result<T, Error>
+//        where T: FromSql + Default
+//    {
+//        let statement = self.client
+//            .lock()
+//            .prepare(sql)
+//            .await?;
+//        self.client
+//            .lock()
+//            .query(&statement, args)
+//            .map_ok(|row|{
+//                T::from_row(&row)
+//            })
+//            .try_collect::<T>().await
+//    }
 }
 
 pub use profugus_derive::FromSql;
 pub use tokio_postgres::{ Row, Error };
+use tokio_postgres::types::ToSql;
 
 pub trait FromSql {
     fn from_row(row: &Row) -> Self;
