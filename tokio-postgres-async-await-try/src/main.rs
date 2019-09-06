ccd0492b50c11f91f6503f527b22e868a4588719
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-09-06 11:08:31 +0200

    make async example work

--- tokio-postgres-async-await-try/src/main.rs
+++ tokio-postgres-async-await-try/src/main.rs
@@ -8,15 +8,19 @@ use bigdecimal::*;
 mod models;
 
 use crate::models::*;
+use futures::TryStreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let (mut client, connection) = tokio_postgres::connect("postgresql://localhost/dellstore2?user=tg", NoTls).await?;
-    let connection = connection.map_err(|e| eprintln!("connection error: {}", e)).await;
+    let connection = connection.map_err(|e| eprintln!("connection error: {}", e)).map(|result|result.unwrap());
+    tokio::spawn(connection);
     let client_ref = Arc::new(Mutex::new(client));
-    let statement = prepare_statement(client_ref.clone(), "SELECT * FROM products ORDER BY prod_id DESC limit 5").await;
-    let result = get_products(client_ref.clone(), statement.unwrap()).await;
 
+    let statement = prepare_statement(client_ref.clone(), "SELECT * FROM products ORDER BY prod_id DESC limit 5").await;
+
+    let result = get_products(client_ref.clone(), statement.unwrap()).await;
+    dbg!(result);
     Ok(())
 }
 
@@ -34,30 +38,21 @@ async fn prepare_statement(client_ref: Arc<Mutex<Client>>, statement: &str) -> B
     }
 }
 
-async fn get_products(client_ref: Arc<Mutex<Client>>, statement: Statement) -> Box<Vec<Option<Product>>> {
+async fn get_products(client_ref: Arc<Mutex<Client>>, statement: Statement) -> Result<Vec<Product>, tokio_postgres::Error> {
     client_ref
         .lock()
         .unwrap()
         .query(&statement, &[]) // returns a stream<Item = Result<Row, tokio_postgres::Error>>
-        .collect()
-        .map(|row_result: Result<Row, tokio_postgres::Error> /*type interference breaks here*/| {
-            // Could be https://github.com/rust-lang-nursery/futures-rs/issues/1833
-            match row_result {
-                Ok(row) => {
-                    Some(Product {
-                        id: row.get(0),
-                        category: row.get(1),
-                        title: row.get(2),
-                        actor: row.get(3),
-                        price: BigDecimal::from(0),
-                        special: row.get(5),
-                        common_prod_id: row.get(6),
-                    })
-                }
-                Err(e) => {
-                    eprintln!("Error in getting a item: {}", e);
-                    None
-                }
+        .map_ok(|row:Row|{
+            Product {
+                id: row.get(0),
+                category: row.get(1),
+                title: row.get(2),
+                actor: row.get(3),
+                price: BigDecimal::from(0),
+                special: row.get(5),
+                common_prod_id: row.get(6),
             }
-        }).collect()
+        })
+        .try_collect::<Vec<_>>().await
 }
