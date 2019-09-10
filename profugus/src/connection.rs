98f46fb87c2fae57f4745353de8f52d2d1d94688
Martijn Groeneveldt <martijn.groeneveldt@gmail.com>
2019-09-10 15:55:42 +0200

    add the last stests

--- profugus/src/connection.rs
+++ profugus/src/connection.rs
@@ -257,6 +257,30 @@ impl PGConnection {
         unimplemented!()
     }
 
+    /// Deletes a item.
+    /// ```
+    /// use profugus::PGConnection;
+    /// use tokio::prelude::*;
+    /// use profugus::FromSql;
+    ///
+    /// #[derive(FromSql, ToSql, Identifiable, Eq, PartialEq, Debug)]
+    /// struct Product {
+    ///     prod_id: i32,
+    ///     title: String
+    /// }
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let conn = PGConnection::new("postgresql://localhost/dellstore2?user=tg").await.unwrap();
+    ///     let new_product = Product {prod_id: 0, title: String::from("Sql insert lesson")};
+    ///     let id = conn.create(new_product).await.unwrap().get_id();
+    ///     let product = conn.query("SELECT prod_id, title from Products where prod_id = $1", &[id]);
+    ///
+    ///     assert_eq!(new_product, product);
+    ///
+    ///     conn.delete(product).await.unwrap();
+    /// }
+    /// ```
     pub async fn delete<T>(item: T) -> Result<(), Error>
     where
         T: Identifiable,
@@ -264,6 +288,34 @@ impl PGConnection {
         unimplemented!()
     }
 
+    /// Deletes a list of items.
+    /// ```
+    /// use profugus::PGConnection;
+    /// use tokio::prelude::*;
+    /// use profugus::FromSql;
+    ///
+    /// #[derive(FromSql, ToSql, Identifiable, Eq, PartialEq, Debug)]
+    /// struct Product {
+    ///     prod_id: i32,
+    ///     title: String
+    /// }
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let conn = PGConnection::new("postgresql://localhost/dellstore2?user=tg").await.unwrap();
+    ///     let new_products = vec!(
+    ///         Product {prod_id: 0, title: String::from("Sql insert lesson")},
+    ///         Product {prod_id: 0, title: String::from("Rust macro lesson")},
+    ///         Product {prod_id: 0, title: String::from("Postgres data types lesson")};
+    ///     );
+    ///     let id = conn.create(new_product).await.unwrap().get_id();
+    ///     let products = conn.query_multiple("SELECT prod_id, title from Products where prod_id = $1", &[id]);
+    ///
+    ///     assert_eq!(new_products, products);
+    ///
+    ///     conn.delete(products).await.unwrap();
+    /// }
+    /// ```
     pub async fn delete_multiple<T>(item: Vec<T>) -> Result<(), Error>
     where
         T: Identifiable,
